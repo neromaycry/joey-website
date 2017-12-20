24bcf05d0a936690bd88d723bd8ade13a09f4b04
unknown <gjwlg@163.com>
2017-12-20 19:40:47 +0800

    修改 背景图片平铺，增加 文字动画效果

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # joey-website
 My personal website repository
+
+http://www.joeykwok.com
---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var title = document.getElementById('name');
+    var txt = new TextFx(title);
+    console.log(txt);
+    txt.hide();
+    title.style.opacity = 1;
+    txt.show('fx3');
+}
